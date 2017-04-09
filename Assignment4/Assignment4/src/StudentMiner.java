c684610c1ca16e7167ef7987d16197474747620e
Giancarlo Giuffra <giancarlo.giuffra.moncayo@gmail.com>
2017-04-09 20:10:21 +0200

    simple implementation of miner

--- Assignment4/Assignment4/src/StudentMiner.java
+++ Assignment4/Assignment4/src/StudentMiner.java
@@ -1,13 +1,18 @@
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
 public class StudentMiner extends Miner {
 
+    private Block heighestBlock;
     private Set<Simulation.Transaction> transactions;
+    private ArrayList<Block> blocksToPublish;
 
     public StudentMiner(Block genesisBlock) {
         super(genesisBlock);
+        heighestBlock = genesisBlock;
         transactions = new HashSet<>();
+        blocksToPublish = new ArrayList<>();
    }
 
     public Set<Simulation.Transaction> getTransactions() {
@@ -22,14 +27,25 @@ public class StudentMiner extends Miner {
     @Override
     public void hearBlock(Block block) {
         transactions.removeAll(block.transactions);
+        if(block.height > heighestBlock.height){
+            heighestBlock = block;
+        }
     }
 
     @Override
     public Block findBlock() {
         long newId = Simulation.getNewId(this);
-        Block fakeBlock = new Block(0, null, new HashSet<>());
-        Block foundBlock = new Block(newId, fakeBlock, transactions);
+        Block foundBlock = new Block(newId, heighestBlock, new HashSet<>(transactions));
         transactions.clear();
+        heighestBlock = foundBlock;
+        blocksToPublish.add(foundBlock);
         return foundBlock;
+    }
+
+    @Override
+    public ArrayList<Block> publishBlock() {
+        ArrayList<Block> blocks = new ArrayList<>(blocksToPublish);
+        blocksToPublish.clear();
+        return blocks;
     }
 }
--- Assignment4/Assignment4/test/StudentMinerTest.java
+++ Assignment4/Assignment4/test/StudentMinerTest.java
@@ -3,9 +3,7 @@ import org.junit.Test;
 
 import java.util.*;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.hasItem;
-import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
@@ -64,6 +62,70 @@ public class StudentMinerTest {
 
         Block foundBlock = miner.findBlock();
         assertThat(foundBlock.transactions, hasItems(tx1, tx2));
+        assertTrue(miner.getTransactions().isEmpty());
+    }
+
+    @Test
+    public void ShouldUseNewIdForNewBlockFound() throws Exception {
+        StudentMiner miner = Miner();
+
+        Simulation.Transaction txInFirstFoundBlock = new Simulation.Transaction();
+        miner.hearTransaction(txInFirstFoundBlock);
+        Block firstFoundBlock = miner.findBlock();
+
+        Simulation.Transaction txInSecondFoundBlock = new Simulation.Transaction();
+        miner.hearTransaction(txInSecondFoundBlock);
+        Block secondFoundBlock = miner.findBlock();
+
+        assertThat(secondFoundBlock.ownerId, not(equalTo(firstFoundBlock.ownerId)));
+    }
+
+    @Test
+    public void ShouldMineOverHighestBlock() throws Exception {
+        StudentMiner miner = Miner();
+
+        Block block = new Block(0, genesisBlock, new HashSet<>());
+        miner.hearBlock(block);
+
+        Simulation.Transaction tx = new Simulation.Transaction();
+        miner.hearTransaction(tx);
+        Block foundBlock = miner.findBlock();
+
+        assertThat(foundBlock.parent, equalTo(block));
+    }
+
+    @Test
+    public void ShouldMineOverHighestBlockEvenIfIMinedIt() throws Exception {
+        StudentMiner miner = Miner();
+        Block firstFoundBlock = miner.findBlock();
+        Block secondFoundBlock = miner.findBlock();
+
+        assertThat(secondFoundBlock.parent, equalTo(firstFoundBlock));
+    }
+
+    @Test
+    public void ShouldPublishMinedBlocksInCorrectOrder() throws Exception {
+        StudentMiner miner = Miner();
+        Block firstFoundBlock = miner.findBlock();
+        Block secondFoundBlock = miner.findBlock();
+
+        ArrayList<Block> blocksToPublish = miner.publishBlock();
+        assertThat(blocksToPublish.size(), equalTo(2));
+        assertThat(blocksToPublish.get(0), equalTo(firstFoundBlock));
+        assertThat(blocksToPublish.get(1), equalTo(secondFoundBlock));
+    }
+
+    @Test
+    public void ShouldNotPublishFoundBlockMoreThanOnce() throws Exception {
+        StudentMiner miner = Miner();
+        Block firstFoundBlock = miner.findBlock();
+
+        ArrayList<Block> firstBlocksToPublish = miner.publishBlock();
+        assertThat(firstBlocksToPublish.size(), equalTo(1));
+        assertThat(firstBlocksToPublish.get(0), equalTo(firstFoundBlock));
+
+        ArrayList<Block> secondBlocksToPublish = miner.publishBlock();
+        assertTrue(secondBlocksToPublish.isEmpty());
     }
 
     private StudentMiner Miner() {
